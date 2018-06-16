5fade4ffadcb92a48ac6b9a6949f447bfaa7ec71
thienminh6397 <1560536@student.hcmus.edu.vn>
2018-06-16 19:49:29 +0700

    chi tiet dau gia lan 1

--- public/js/XuLyDuLieu/index.js
+++ public/js/XuLyDuLieu/index.js
@@ -27,7 +27,8 @@ $(document).ready(function () {
             url: "/allitem",
             success: (data) => {
                 var result = data.kq;
-                $("#allitem").append("<section class='col-xs-12 col-sm-6 col-md-6 col-lg-6 grid s1' ></section>");
+                let msp = result[0].MaSanPham;
+                $("#allitem").append("<section class='col-xs-12 col-sm-6 col-md-6 col-lg-6 grid s1' onclick='ChiTiet(" + msp + " )' ></section>");
                 $(".s1").append("<figure class='effect-oscar f1'></figure>")
                 $(".f1").append("<img src=" + result[0].HinhDaiDien + " class='img-responsive' width='100%'/>");
                 $(".f1").append("<figcaption class='fi1'></figcaption>");
@@ -41,7 +42,8 @@ $(document).ready(function () {
                 $("#allitem").append("<div class='clearfix'></div>")
                 for (var i = 1; i < result.length; i++) {
                     let temp = i + 1;
-                    $("#allitem").append("<section class='col-xs-12 col-sm-6 col-md-6 col-lg-6 grid s" + temp + "' ></section>");
+                    let msp = result[i].MaSanPham;
+                    $("#allitem").append("<section class='col-xs-12 col-sm-6 col-md-6 col-lg-6 grid s" + temp + "' onclick='ChiTiet(" + msp + ")' ></section>");
                     $(".s" + temp + "").append("<figure class='effect-oscar f" + temp + "'></figure>")
                     $(".f" + temp + "").append("<img src=" + result[i].HinhDaiDien + " class='img-responsive' width='100%'/>");
                     $(".f" + temp + "").append("<figcaption class='fi" + temp + "'></figcaption>");
@@ -69,3 +71,37 @@ $(document).ready(function () {
         })
     })
 })
+
+function ChiTiet(msp) {
+    $("main").empty();
+    $('.anchodgct').empty();
+    $.ajax({
+        type: "GET",
+        url: "/chitiet/" + msp,
+        success: (data) => {
+            var kq = data.kq;
+            $("main").append("<div class='work-details w2'></div>");
+            $(".w2").append("<div class='row r2'></div>");
+            $('.r2').append(`<div class="col-xs-12 col-sm-12 col-md-4 c"></div>`);
+            $('.c').append("<header role='work-title' class='chitiet2'></header>");
+            $('.chitiet2').append(`<h2>${kq.TenSanPham}</h2>`);
+            $('.chitiet2').append(`<div class="chutoty">Giá hiện tại:<span class="giatien">4K</span></div>`);
+            $('.chitiet2').append(`<div class="chutoty">Thời gian kết thúc:<span class="thoigianconlai">00:10:00</span></div>`);
+            $('.chitiet2').append(`<div class="chutoty">Giá bạn đặt:<input type="text" name="giadat" id="giatdat" size="100"></div><br>`);
+            $('.chitiet2').append(`<button type="button" name="btnDaugia" onclick='DauGia();' class="btn btn-success">Đấu giá<span class="glyphicon glyphicon-ok"></span></button>`);
+            $('.r2').append(`<div class="col-xs-12 col-sm-12 col-md-8 ttsp"></div>`)
+            $('.ttsp').append(`<section class='section'><h4>Thông tin sản phẩm.</h4></section>`);
+            $('.section').append(`<p>${kq.DacTa}</p>`);
+            $('.section').append(`<p><strong>Loại</strong><br/>${kq.TenLoaiSanPham}</p>`);
+            $('.w2').append(`<img src="${kq.DuongDan}/1.jpg" />`);
+            $('.w2').append(`<img src="${kq.DuongDan}/2.jpg" />`);
+            $("input[name='giadat']").TouchSpin({
+                verticalbuttons: true
+            });
+        }
+    })
+}
+
+function DauGia() {
+
+}
--- routes/chitiet.js
+++ routes/chitiet.js
@@ -1,7 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+var conn = require("../public/connect/connect");
 router.get('/', (req, res) => {
 
+})
+router.get('/:msp', (req, res) => {
+    var msp = req.params.msp;
+    conn.getConnection((err, connection) => {
+        if (err) throw err;
+        var sql = `select * from sanpham sp join loaisanpham lsp on sp.MaLoaiSanPham=lsp.MaLoaiSanPham join hinh h on h.MaSanPham = sp.MaSanPham where sp.MaSanPham = ${msp}`;
+        connection.query(sql, (err, result) => {
+            connection.release();
+            if (err) console.log("loi");
+            else {
+                res.send({
+                    'kq': result[0]
+                });
+            }
+        })
+    })
 })
 module.exports = router;
